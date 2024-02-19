1550663625d5adc27f0f9f296df5fc7fac3088b1
Laurids Kern <me@lauridskern.com>
2024-02-19 10:45:58 +0100

    -

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NoSSRWrapper from "@/components/noSSRWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{<NoSSRWrapper>children</NoSSRWrapper>}</body>
     </html>
   );
 }
