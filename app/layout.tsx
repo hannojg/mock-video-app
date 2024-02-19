2b8139e975a9dedaf8d0b01f37c6a6e0aefed2dc
Laurids Kern <me@lauridskern.com>
2024-02-19 18:30:45 +0100

    Add @vercel/analytics package and component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NoSSRWrapper from "@/components/noSSRWrapper";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{<NoSSRWrapper>{children}</NoSSRWrapper>}</body>
+      <body className={inter.className}>
+        {
+          <NoSSRWrapper>
+            {children}
+            <Analytics />
+          </NoSSRWrapper>
+        }
+      </body>
     </html>
   );
 }
