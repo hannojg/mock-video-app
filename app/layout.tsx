06f6f193ca5559ff0738135278df36cdc39da39f
Laurids Kern <me@lauridskern.com>
2024-02-19 10:47:24 +0100

    -

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{<NoSSRWrapper>children</NoSSRWrapper>}</body>
+      <body className={inter.className}>{<NoSSRWrapper>{children}</NoSSRWrapper>}</body>
     </html>
   );
 }
