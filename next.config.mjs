6f8010fbbf6ebddcfb9ca05b5e20ddb5801931c4
Laurids Kern <me@lauridskern.com>
2024-05-03 20:26:18 +0200

    keep 60fps?

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,15 @@ const nextConfig = {
   // async headers() {
   //   return [
   //     {
-  //       source: '/',
+  //       source: "/",
   //       headers: [
   //         {
-  //           key: 'Cross-Origin-Embedder-Policy',
-  //           value: 'require-corp',
+  //           key: "Cross-Origin-Embedder-Policy",
+  //           value: "require-corp",
   //         },
   //         {
-  //           key: 'Cross-Origin-Opener-Policy',
-  //           value: 'same-origin',
+  //           key: "Cross-Origin-Opener-Policy",
+  //           value: "same-origin",
   //         },
   //       ],
   //     },
